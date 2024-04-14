010cb4909e956f2f33ae445b57a5354aac1e5260
RBCodeJourney <refatullahbiswas@gmail.com>
2024-04-14 13:51:05 +0530

    elips-area-calculator added

--- js/rectangle-area.js
+++ js/rectangle-area.js
@@ -1,3 +1,26 @@
-function calculateRectangleArea(){
-    console.log('calculate rectAngel area')
+function calculateRectangleArea() {
+    // get length of the rectangle 
+    const lengthInput = document.getElementById('rectangle-length');
+    const lengthText = lengthInput.value;
+    const length = parseFloat(lengthText);
+    console.log(length);
+
+    //  get width of the rectangle 
+
+    const widthInput = document.getElementById('rectangle-width');
+    const widthText = widthInput.value;
+    const width = parseFloat(widthText);
+    console.log(width);
+
+
+    // calculate rectangle are 
+
+    const area = length * width;
+    console.log('area of the rectangle', area);
+
+
+    // display rectangle are 
+
+    const rectangleAreaSpan = document.getElementById('rectangle-area');
+    rectangleAreaSpan.innerText = area;
 }
